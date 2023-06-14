9d0104b8a75bdd177080cc3375c21ac6e376b158
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2023-06-14 11:38:29 -0400

    Agregacion de la autenticacion

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
+using System.Text.Json;
+
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+
+using Global.DTOs;
+using Global.Service;
+
+using Blazored.SessionStorage;
+using Blazored.LocalStorage;
+
 using Calidad.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +16,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredSessionStorage();
+// builder.Services.AddBlazoredSessionStorage(config =>
+// {
+//     config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+//     config.JsonSerializerOptions.IgnoreNullValues = true;
+//     config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+//     config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+//     config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+//     config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+//     config.JsonSerializerOptions.WriteIndented = false;
+// });
+
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllersWithViews();
+builder.Services.AddOptions();  
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<global::Microsoft.AspNetCore.Components.Authorization.AuthenticationStateProvider,global:: Global.Service.Autenticacion.CustomAuthStateProvider>();
+
+
+
+//Todo: Pendiente para borrar
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
